c0093031a7cf38a4334a9996f3d4cb32a4371fca
= <ssh.xero@gmail.com>
2025-04-07 18:50:36 +0100

    chore(ui): Revert to geist sans font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,41 @@
 import type {Metadata} from 'next';
-import localFont from 'next/font/local';
+import {Geist, Geist_Mono} from 'next/font/google';
+// import localFont from 'next/font/local';
 import './globals.css';
 import meta from '@/data/meta';
 import ThemeSwitchProvider from '@/providers/ThemeSwitchProvider';
 
-const dankMono = localFont({
-    src: [
-        {
-            path: '../public/fonts/DankMonoNerdFontMono-Regular.otf',
-            weight: '400',
-            style: 'normal',
-        },
-        {
-            path: '../public/fonts/DankMonoNerdFont-Italic.otf',
-            weight: '400',
-            style: 'italic',
-        },
-        {
-            path: '../public/fonts/DankMonoNerdFont-Bold.otf',
-            weight: '700',
-            style: 'normal',
-        },
-    ],
-    variable: '--font-dank-mono',
-    display: 'swap',
+const geistSans = Geist({
+    variable: '--font-geist-sans',
+    subsets: ['latin'],
 });
+
+const geistMono = Geist_Mono({
+    variable: '--font-geist-mono',
+    subsets: ['latin'],
+});
+
+// const dankMono = localFont({
+//     src: [
+//         {
+//             path: '../public/fonts/DankMonoNerdFontMono-Regular.otf',
+//             weight: '400',
+//             style: 'normal',
+//         },
+//         {
+//             path: '../public/fonts/DankMonoNerdFont-Italic.otf',
+//             weight: '400',
+//             style: 'italic',
+//         },
+//         {
+//             path: '../public/fonts/DankMonoNerdFont-Bold.otf',
+//             weight: '700',
+//             style: 'normal',
+//         },
+//     ],
+//     variable: '--font-dank-mono',
+//     display: 'swap',
+// });
 
 const ogImageUrl = new URL(`${meta.url}/api/og`)
 ogImageUrl.searchParams.append("title", "David Dada")
@@ -67,7 +78,7 @@ export default function RootLayout({children}: Readonly<{
     return (
         <html lang="en">
         <body
-            className={`${dankMono.variable} font-dank-mono antialiased`}
+            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
         <ThemeSwitchProvider>
             {children}
