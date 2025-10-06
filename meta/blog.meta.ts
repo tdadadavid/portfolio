8c835bcf29d4661ace6722482fd104f61eb8e323
tdadadavid <davidtofunmidada@gmail.com>
2025-10-06 03:27:18 +0100

    clean up
    Signed-off-by: tdadadavid <davidtofunmidada@gmail.com>

--- meta/blog.meta.ts
+++ meta/blog.meta.ts
@@ -4,40 +4,13 @@ import { BlogMetadata } from '@/types/blog.type';
 // automatically. 
 // The object key must still be set to the directory name.
 export const blogMetadata: Record<string, BlogMetadata> = {
-    'distributed-sys': {
-        title: 'Distributed Systems I',
-        summary: 'Introduction to Distributed Systems [The Problem]',
-        publishedOn: '2025-05-22T22:00:18.147Z',
+    'raft': {
+        title: 'Raft',
+        summary: 'Raft',
+        publishedOn: '2025-10-06T02:10:44',
         year: '2025',
         slug: '',
-        tags: ['distributed-systems', 'storage', 'communication', 'go'],
+        tags: ['distributed-systems', 'consensus', 'go', 'raft'],
         status: 'done',
-    },
-    'the-solution': {
-        title: 'Distributed Systems II',
-        summary: 'Introduction to Distributed Systems [The Solution]',
-        publishedOn: '2025-05-22T22:00:18.147Z',
-        year: '2025',
-        slug: '',
-        tags: ['distributed-systems', 'storage', 'communication'],
-        status: 'draft',
-    },
-    'false-assumptions': {
-        title: 'Distributed Systems III',
-        summary: 'Introduction to Distributed Systems [Assumptions]',
-        publishedOn: '2025-05-22T22:00:18.147Z',
-        year: '2025',
-        slug: '',
-        tags: ['distributed-systems', 'network', 'communication'],
-        status: 'draft',
-    },
-    'map-reduce': {
-        title: 'MapReduce',
-        summary: 'Distributed System Framework',
-        publishedOn: '2025-05-22T22:00:18.147Z',
-        year: '2025',
-        slug: '',
-        tags: ['distributed-systems', 'go', 'mapreduce'],
-        status: 'draft',
     },
 };
