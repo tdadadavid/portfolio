df67eec6d7241d0f25f20e0a047da187dcc2a9e3
tdadadavid <davidtofunmidada@gmail.com>
2025-10-29 12:38:57 +0100

    fix dns blog issues
    Signed-off-by: tdadadavid <davidtofunmidada@gmail.com>

--- meta/blog.meta.ts
+++ meta/blog.meta.ts
@@ -19,7 +19,7 @@ export const blogMetadata: Record<string, BlogMetadata> = {
         publishedOn: '2025-10-27T19:16:03',
         year: '2025',
         slug: 'dns-server-in-go',
-        tags: ['dns', 'go', 'eventual consistency'],
+        tags: ['dns', 'go'],
         status: 'in-progress',
     },
 };
